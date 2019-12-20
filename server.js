c1ab1cbb31a2f9fe78792d489d63058f94289579
vsharm <vsharm@umich.edu>
2019-12-20 13:36:15 -0800

    complete

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const people = require('./people.json');
+
 const app = express();
 
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        title: 'Homepage',
+        people: people.profiles
+    });
 });
 
-app.set('view engine', 'pug');
+app.get('/profile', (req, res) => {
+  const person = people.profiles.find(p => p.id === req.query.id);
+  res.render('profile', {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
+});
 
 const server = app.listen(7000, () => {
     console.log('Express running -> PORT ${server.address().port}');
